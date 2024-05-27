b740962dcfe85850fd880e986bd373ed98952ae5
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-05-27 22:00:38 +0300

    changed comment

--- app/src/main/java/com/example/sellstuff/MainActivity.kt
+++ app/src/main/java/com/example/sellstuff/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SellStuffTheme {
-                // A surface container using the 'background' color from the theme
+                // this is a test comment
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
